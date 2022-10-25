b6b0b48f919c53585c955a10e9fdd82edbb8d3f8
tomm8231 <tomm8231@stud.kea.dk>
2022-10-25 10:19:29 +0200

    styles.css, application.properties, GetMapping in HomeController set up

--- src/main/java/com/example/wishlist/controller/HomeController.java
+++ src/main/java/com/example/wishlist/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.example.wishlist.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+  @GetMapping("")
+  public String index() {
+    return "index";
+  }
 }
